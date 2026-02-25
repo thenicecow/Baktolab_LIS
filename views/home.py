bc18a5692385ebff2897bb67cfd64a0fefdce227
Kevin Engehausen <engehkev@students.zhaw.ch>
2026-02-25 10:21:46 +0100

    Emailadresse Hinzugefügt

--- views/home.py
+++ views/home.py
@@ -6,7 +6,7 @@ st.title("Meine erste Streamlit App")
 
 """
 Diese App wurde von folgenden Personen entwickelt:
-- Kevin Engehausen (engehkev@students.zhaw.ch)
+- Kevin Engehausen (engehkev@students.zhaw.ch) 
 - Melina Kraus (krausme1@students.zhaw.ch)
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
