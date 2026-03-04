ae34253f121f74171ac62aec206ec9cf47043dd3
Kevin Engehausen <engehkev@students.zhaw.ch>
2026-03-04 22:36:11 +0100

    Hauptseite angepasst

--- views/home.py
+++ views/home.py
@@ -13,7 +13,6 @@ Diese App wurde von folgenden Personen entwickelt:
 - Kevin Engehausen (engehkev@students.zhaw.ch) 
 - Melina Kraus (krausme1@students.zhaw.ch)
 
-Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
 Autor: Samuel Wehrli (wehs@zhaw.ch)
 """)
