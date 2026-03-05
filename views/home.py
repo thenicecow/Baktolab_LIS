694c0327424117e452746c5fe08c47a0fe11d972
Kevin Engehausen <engehkev@students.zhaw.ch>
2026-03-05 22:36:38 +0100

    home angepasst

--- views/home.py
+++ views/home.py
@@ -3,9 +3,10 @@ import streamlit as st
 st.title("Rechner Resistenzmonitoring")
 
 st.markdown("""
-Diese App berechnet die Antibiotika-Resistenzrate ausgewählter bakterieller Erreger anhand der Anzahl getesteter und resistenter Isolate in einem definierten Zeitraum.  
+Diese App berechnet die Antibiotika-Resistenzrate ausgewählter bakterieller Erreger anhand der Anzahl getesteter und resistenter Isolate einer Asuwertungsperiode (bspw. April 2023).  
 Die Resistenzrate wird in Prozent dargestellt und mit einem einfachen Ampelsystem bewertet.  
-Zusätzlich werden bei bestimmten Kombinationen von Erreger und Antibiotikaklasse Hinweise zu möglichen multiresistenten Erregern angezeigt.
+Zusätzlich werden bei bestimmten Kombinationen von Erreger und Antibiotikaklasse Hinweise zu möglichen multiresistenten Erregern angezeigt. 
+Es können mehrere Auswertungen durchgeführt und in einer Tabelle gespeichert werden, um Trends über die Zeit zu verfolgen.
 """)
 
 st.markdown("""
