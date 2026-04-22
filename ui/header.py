7d8e15b4f3739b71cfdf70ad4e848729f67c6d4f
haschdav <haschdav@students.zhaw.ch>
2026-04-22 09:28:33 +0200

    doc string

--- ui/header.py
+++ ui/header.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
 def show_header(title=None):
+    """Displays the application header with an optional title and a logo.
+    Args:
+        title (str, optional): The title to display in the header. Defaults to None.
+    """
     col1, col2 = st.columns([5, 1])
 
     with col1:
